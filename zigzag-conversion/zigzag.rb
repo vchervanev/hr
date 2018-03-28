3a3ac0d64876440a0a04e62874280ccdf5b9e89b
Vladimir Chervanev <vchervanev.dev@gmail.com>
2018-03-28 23:07:09 +1100

    raw wip

--- zigzag-conversion/zigzag.rb
+++ zigzag-conversion/zigzag.rb
@@ -1,23 +1,43 @@
+class PositionCalculator
+  def initialize(step:, rows:, size:)
+    @step = step
+    @rows = rows
+    @size = size
+    @wave = -1
+    @row = 0
+    @first = nil
+  end
+
+  def next
+    if middle_row?
+
+    else
+      @wave += 1
+    end
+  end
+
+  private
+
+  def middle_row?
+    @row > 0 && @row < rows - 1
+  end
+end
+
 # @param {String} s
 # @param {Integer} num_rows
 # @return {String}
 def convert(s, num_rows)
   return s if num_rows == 1
-  delta = 0
   step = 2 * num_rows - 2
-  prefix = 0
   result = Array.new(s.size)
+
+  pc = PositionCalculator.new(step: step, rows: num_rows, size: s.size)
   s.size.times do |k|
-    candidate = (k - prefix) * step + delta
-    if candidate >= s.size
-      prefix = k
-      delta += 1
-      candidate = (k - prefix) * step + delta
-    end
-    result[k] = s[candidate]
+    result[k] = s[pc.next]
   end
   result.join
 end
+
 if defined? RSpec
   RSpec.describe do
     use_cases =
